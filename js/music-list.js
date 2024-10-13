6ad269fc44e7628652b1e8189ff56a86f84fc558
rahul1259s <rhj81680@gmail.com>
2024-10-13 20:52:06 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+  {
+    name: "You and Me",
+    artist: "Shubh",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Satranga",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "One Love",
+    artist: "Shubh",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Jo Tume mere ho",
+    artist: "Anuv jain",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Husn",
+    artist: "Anuv jain",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Prada",
+    artist: "Jass Manak",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "Viah",
+    artist: "Jass Manak",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "Tera mera viah",
+    artist: "Jass manak",
+    img: "music-8",
+    src: "music-8",
+  },
+];
